44963acdfb6dde1123688713cf3653b2252c5361
xaiam <i.miyahara@xearts.jp>
2020-05-02 21:24:58 +0900

    createFormBuiler()の引数をInquiryエンティティオブジェクトにしたのでそのエンティティの戻り値を?stringなどにした。

---
+++ src/Form/InqueryFormType.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Inquiry;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class InqueryFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('email')
+            ->add('tel')
+            ->add('type')
+            ->add('content')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Inquiry::class,
+        ]);
+    }
+}
---
+++ src/Repository/InquiryRepository.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Inquiry;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Inquiry|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Inquiry|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Inquiry[]    findAll()
+ * @method Inquiry[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class InquiryRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Inquiry::class);
+    }
+
+    // /**
+    //  * @return Inquiry[] Returns an array of Inquiry objects
+    //  */
+    /*
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('i.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    */
+
+    /*
+    public function findOneBySomeField($value): ?Inquiry
+    {
+        return $this->createQueryBuilder('i')
+            ->andWhere('i.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
